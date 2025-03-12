3708e86c8091786451e4a2dfe8617eaba166934b
Cristhian Ortega <cristhian.ortega@ibtmx.com>
2025-03-11 23:27:49 -0600

    Se ha añadido el formulario para crear Personas.

--- CRUD_Example/Controllers/PersonsController.cs
+++ CRUD_Example/Controllers/PersonsController.cs
@@ -2,19 +2,23 @@
 using ServiceContracts;
 using ServiceContracts.DTO;
 using ServiceContracts.Enums;
+using Services;
 
 namespace CRUD_Example.Controllers
 {
+    [Route("[controller]")] //Se añade como prefijo a todas las rutas
     public class PersonsController : Controller
     {
         private readonly IPersonsService _personsService;
+        private readonly ICountriesService _countriesService;
 
-        public PersonsController(IPersonsService personsService)
+        public PersonsController(IPersonsService personsService, ICountriesService countriesService)
         {
             _personsService = personsService;
+            _countriesService = countriesService;
         }
 
-        [Route("persons/index")]
+        [Route("[action]")]
         [Route("/")]
         public IActionResult Index(string searchBy, string? searchString, string sortBy = nameof(PersonResponse.PersonName), SortOrderOptions sortOrder = SortOrderOptions.ASC)
         {
@@ -60,6 +64,32 @@ namespace CRUD_Example.Controllers
             ViewBag.CurrentSortBy = sortBy;
             ViewBag.CurrentSortOrder = sortOrder.ToString();
             return View(sortedPersons); //Views/Persons/Index.cshtml
+        }
+
+        [Route("[action]")]
+        [HttpGet]
+        public IActionResult Create()
+        {
+            List<CountryResponse> countries = _countriesService.GetAllCountries();
+            ViewBag.Countries = countries;
+                
+            return View();
+        }
+
+        [Route("[action]")]
+        [HttpPost]
+        public IActionResult Create(PersonAddRequest personAddRequest)
+        {
+            if (!ModelState.IsValid)
+            {
+                List<CountryResponse> countries = _countriesService.GetAllCountries();
+                ViewBag.Countries = countries;
+
+                ViewBag.Errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage).ToList();
+                return View();
+            }
+            PersonResponse personPerson = _personsService.AddPerson(personAddRequest);
+            return RedirectToAction("Index", "Persons");
         }
     }
 }
